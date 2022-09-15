2b6e1506223ddfd8aa21c5021f9bccb085f93d4c
Sana Mumtaz <35191030+sanamumtaz@users.noreply.github.com>
2022-09-15 13:02:52 +0500

    upgrade ecmaVersion to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 2020,
+        ecmaVersion: 'latest',
         sourceType: 'module'
     },
     settings: {
